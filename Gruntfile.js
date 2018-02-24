7b73a9b529d750f640ef33ae5f7200de41eb36a2
Jiří Brychta <jirbry@gmail.com>
2018-02-24 15:35:26 +0100

    [FIX] grunt cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -153,6 +153,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-browser-sync');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
 
